23d9ef3a4010dd4f0c6258a68eb430d5521527fe
Leonidas <leonidas.ramirez.q@gmail.com>
2021-12-16 21:01:37 -0500

    Created the node.js server

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const router = express.Router();
+
+var app = express();
+
+app.use(router);
+
+router.get("/message", function (req, res) {
+  res.send("Lista de mensajes");
+});
+
+router.post("/message", function (req, res) {
+  res.send("mensaje añadido");
+});
+/*
+app.use("/", function (req, res) {
+  res.send("Hola");
+});
+*/
+
+app.listen(3000);
+console.log("La aplicación está escuchando en http://localhost:3000");
